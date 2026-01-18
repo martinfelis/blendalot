67acbb35b333c73264f3d56a61ef6d6ab98e9200
Martin Felis <martin@fysx.org>
2026-01-18 23:27:59 +0100

    Started working on editor to create and test more complex graphs.

---
+++ demo/addons/blendalot/blendalot_plugin.gd
@@ -0,0 +1,45 @@
+@tool
+extends EditorPlugin
+
+const MainPanel = preload("res://addons/blendalot/blendalot_main_panel.tscn")
+
+var main_panel_instance
+
+func _enable_plugin() -> void:
+	# Add autoloads here.
+	pass
+
+
+func _disable_plugin() -> void:
+	# Remove autoloads here.
+	pass
+
+
+func _enter_tree() -> void:
+	main_panel_instance = MainPanel.instantiate()
+	# Add the main panel to the editor's main viewport.
+	EditorInterface.get_editor_main_screen().add_child(main_panel_instance)
+	# Hide the main panel. Very much required.
+	_make_visible(false)
+
+
+func _exit_tree() -> void:
+	if main_panel_instance:
+		main_panel_instance.queue_free()
+
+
+func _has_main_screen():
+	return true
+
+
+func _make_visible(visible):
+	if main_panel_instance:
+		main_panel_instance.visible = visible
+
+
+func _get_plugin_name():
+	return "Blendalot"
+
+
+func _get_plugin_icon():
+	return EditorInterface.get_editor_theme().get_icon("Node", "EditorIcons")
