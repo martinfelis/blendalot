63d3d57fb7dc8185af78975b21e58cbbd18ace4d
Martin Felis <martin@fysx.org>
2026-04-26 14:53:23 +0200

    Started refactoring to add GDExtension support.

--- register_types.cpp
+++ register_types.cpp
@@ -1,29 +1,52 @@
 #include "register_types.h"
 
-#include "blendalot_animation_graph.h"
-#include "blendalot_blend_tree.h"
-#include "blendalot_state_machine.h"
-
+#ifdef BLENDALOT_MODULE
 #include "core/object/class_db.h"
+#endif
 
-void initialize_blendalot_animgraph_module(ModuleInitializationLevel p_level) {
+#ifdef BLENDALOT_GDEXTENSION
+#include <gdextension_interface.h>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+using namespace godot;
+#endif
+
+#include "src/example_class.h"
+
+void initialize_blendalot_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
-	ClassDB::register_class<BLTAnimationGraph>();
-	ClassDB::register_class<BLTAnimationNode>();
-	ClassDB::register_class<BLTAnimationNodeOutput>();
-	ClassDB::register_class<BLTBlendTree>();
-	ClassDB::register_class<BLTAnimationNodeSampler>();
-	ClassDB::register_class<BLTAnimationNodeTimeScale>();
-	ClassDB::register_class<BLTAnimationNodeBlend2>();
-	ClassDB::register_class<BLTStateMachine>();
-	ClassDB::register_class<BLTStateMachineTransition>();
+	GDREGISTER_CLASS(ExampleClass);
+
+	//	ClassDB::register_class<BLTAnimationGraph>();
+	//	ClassDB::register_class<BLTAnimationNode>();
+	//	ClassDB::register_class<BLTAnimationNodeOutput>();
+	//	ClassDB::register_class<BLTBlendTree>();
+	//	ClassDB::register_class<BLTAnimationNodeSampler>();
+	//	ClassDB::register_class<BLTAnimationNodeTimeScale>();
+	//	ClassDB::register_class<BLTAnimationNodeBlend2>();
+	//	ClassDB::register_class<BLTStateMachine>();
+	//	ClassDB::register_class<BLTStateMachineTransition>();
 }
 
-void uninitialize_blendalot_animgraph_module(ModuleInitializationLevel p_level) {
+void uninitialize_blendalot_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
-	// Nothing to do here in this example.
 }
+
+#ifndef BLENDALOT_MODULE
+extern "C" {
+// Initialization
+GDExtensionBool GDE_EXPORT blendalot_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+	GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+	init_obj.register_initializer(initialize_blendalot_module);
+	init_obj.register_terminator(uninitialize_blendalot_module);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+	return init_obj.init();
+}
+}
+#endif
